b267327050b0b6f375a501ae9c204933f1893abe
carloseli0297 <carloseli0297@gmail.com>
2025-04-12 20:52:15 +0000

    Initialize Login.

---
+++ app/auth/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import React from "react";
+
+function Login() {
+  return (
+    <div>
+      <div>
+        <Image src={"/logo.png"} alt="logo" width={100} height={100} />
+      </div>
+    </div>
+  );
+}
+
+export default Login;
